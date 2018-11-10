2b6d490f4b44096835c5e6918ac99eb400e29f4f
Lal <lala@vanderbilt.edu>
2018-11-10 15:06:19 -0600

    merging changes, cylinders still not quite working

---
+++ Assets/Cylinder1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cylinder1 : MonoBehaviour
+{
+
+    public Rigidbody Partial3;
+    public Transform Cylinder1Loc;
+
+    bool collisionAlreadyOccurred = false;
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Cylinder1 loc start: " + Cylinder1Loc.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    //private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("other:" + other);
+        if (other.gameObject.CompareTag("Cylinder2child") && !collisionAlreadyOccurred)
+        {
+            // Check alignment
+
+            Vector3 othervec = other.bounds.max - other.bounds.min;
+            Debug.Log("othervec:" + othervec);
+            collisionAlreadyOccurred = true;
+            Debug.Log("Collision with Cylinder 2 detected \n");
+            Debug.Log("Cylinder1 loc: " + Cylinder1Loc.position);
+            Cylinder1Loc = gameObject.transform;
+            Destroy(gameObject.transform.parent.transform.parent.gameObject);
+            Destroy(other.gameObject.transform.parent.transform.parent.gameObject);
+
+            //Debug.Log(Sphere1Loc.position);
+            Rigidbody partialInstance;
+            partialInstance = Instantiate(Partial3, Cylinder1Loc.position, Cylinder1Loc.rotation) as Rigidbody;
+            //partialInstance = Instantiate(Partial3, gameObject.transform.position, gameObject.transform.rotation) as Rigidbody;
+            Debug.Log("Partial 3 loc: " + partialInstance.transform.position);
+        }
+    }
+}
